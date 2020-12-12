3285ba9f8c7270ccb174dec3518914ee913c79a7
KevinBretonnelCohen <kevin.cohen@gmail.com>
2020-12-12 16:32:22 -0700

    Improved anchors in the regular expressions
    This version has improved anchors in the regular expressions. That keeps some of the low-count miscellaneous annotations from getting through.

--- translatorNormalizeClassLabels.pl
+++ translatorNormalizeClassLabels.pl
@@ -8,6 +8,7 @@
 use strict 'vars';
 
 # test data: 
+# turn on debugging when running the test data, because that's how the gold outputs are stored
 
 # set to 1 for debugging output, or to 0 to suppress same
 #my $DEBUG = 1;
@@ -16,38 +17,38 @@ my $DEBUG = 0;
 while (my $line = <>) { 
 
   # PASS THROUGH UNCHANGED: Y and N
-  if ($line =~ /null\t[0-9]+\tY/o) {
+  if ($line =~ /null\t[0-9]+\tY\t/o) {
     $DEBUG && print "UNCHANGED-Y ";
     print $line; next;
   } 
-  if ($line =~ /null\t[0-9]+\tN/o) {
+  if ($line =~ /null\t[0-9]+\tN\t/o) {
     $DEBUG && print "UNCHANGED-N ";
     print $line; next;
   }
 
   # LABELS THAT NEED TO BE NORMALIZED TO Y or N
   # "anaphora" -> Y
-  if ($line =~ s/(null\t[0-9]+\t)anaphora/$1Y/o) {
+  if ($line =~ s/(null\t[0-9]+\t)anaphora\t/$1Y\t/o) {
   $DEBUG && print "ANAPHORA ";  
   print $line; next;
   }
   # "entity" -> N
-  if ($line =~ s/(null\t[0-9]+\t)entity/$1N/o) {
+  if ($line =~ s/(null\t[0-9]+\t)entity\t/$1N\t/o) {
     $DEBUG && print "ENTITY ";  
     print $line; next;
   }
   # "speculation" -> N
-  if ($line =~ s/(null\t[0-9]+\t)speculation/$1N/o) {
+  if ($line =~ s/(null\t[0-9]+\t)speculation\t/$1N\t/o) {
     $DEBUG && print "SPECULATION ";
     print $line; next;
   }
   # "process" -> N
-  if ($line =~ s/(null\t[0-9]+\t)process/$1N/o) {
+  if ($line =~ s/(null\t[0-9]+\t)process\t/$1N\t/o) {
     $DEBUG && print "PROCESS ";
     print $line; next;
   }
   # "other.relation" -> N
-  if ($line =~ s/(null\t[0-9]+\t)other.relation/$1N/o) {
+  if ($line =~ s/(null\t[0-9]+\t)other.relation\t/$1N\t/o) {
     $DEBUG && print "OTHER.RELATION ";
     print $line; next;
   }
@@ -58,7 +59,7 @@ while (my $line = <>) {
   # "D(ecrease)" -> Y
 
   # "U(nspecified directionality)" -> Y
-  if ($line =~ s/(null\t[0-9]+\t)U/$1N/o) {
+  if ($line =~ s/(null\t[0-9]+\t)U\t/$1N\t/o) {
     #print $line; 
     next;
   }

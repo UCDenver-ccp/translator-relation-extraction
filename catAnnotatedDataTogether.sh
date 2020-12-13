bfd9204159a49d257bd323bba2e7d691433c5002
KevinBretonnelCohen <kevin.cohen@gmail.com>
2020-12-13 08:25:37 -0700

    Initial commit
    Version used to generate the concatenated files that the data extraction/counting script needs to generate the counts of annotations for each relation.

---
+++ catAnnotatedDataTogether.sh
@@ -0,0 +1,7 @@
+#cat drug-gene/*.tsv > concatenated-annotations/concatenated.drug-gene.tsv
+cat gene-gene/*.tsv > concatenated-annotations/concatenated.gene-gene.tsv
+#cat drug-disease/*.tsv > concatenated-annotations/concatenated.drug-disease.tsv
+#cat gene-disease/*.tsv > concatenated-annotations/concatenated.gene-disease.tsv
+#cat symptom-disease/*.tsv > concatenated-annotations/concatenated.symptom-disease.tsv
+#cat gene-location/*.tsv > concatenated-annotations/concatenated.gene-location.tsv
+

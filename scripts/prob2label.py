316f4d490585fbfcf1903f7ac4390480b1007829
Bill Baumgartner <bill.baumgartner@gmail.com>
2022-10-11 13:26:29 -0600

    update labels for craft_relations

--- scripts/prob2label.py
+++ scripts/prob2label.py
@@ -29,6 +29,9 @@ elif model_key == 'bl_gene_loss_gain_of_function_to_disease':
 elif model_key == 'bl_gene_to_expression_site':
     labels[0]="expressed_in"
     labels[1]="false"
+elif model_key == 'craft_relations':
+    labels[0]="bears_constitution_of_or_situatedness_at_or_possession_by_or_derivation_from"
+    labels[1]="false"
 # elif model_key == 'bl_gene_to_go_term':
 #   labels[0]="????"
 #   labels[1]="????"

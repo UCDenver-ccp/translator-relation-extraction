f73985279090f8e08609e3d70dc7c1dcbfe51605
Bill Baumgartner <bill.baumgartner@gmail.com>
2022-10-11 11:55:26 -0600

    initial commit of craft assertion -to- bert training code

---
+++ data/craft_relations/code/java/src/main/java/edu/cuanschutz/ccp/bert_craft/CraftToBertRelationTrainingFileMain.java
@@ -0,0 +1,153 @@
+package edu.cuanschutz.ccp.bert_craft;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.tools.ant.util.StringUtils;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+import edu.ucdenver.ccp.datasource.fileparsers.obo.OntologyUtil;
+
+public class CraftToBertRelationTrainingFileMain {
+
+	private static final String OWL_UNNAMED_NS = "http://www.owl-ontologies.com/unnamed.owl#";
+	private static final String OBO_NS = "http://purl.obolibrary.org/obo/";
+	private static final String CCP_EXT_NS = "http://ccp.cuanschutz.edu/obo/ext/";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		File craftBaseDirectory = new File("/Users/bill/projects/craft-shared-task/craft.git");
+		File craftAnnotationDirectory = new File(
+				"/Users/bill/projects/ncats-translator/relations/craft-relations/Annotations");
+		File craftOntologyFile = new File(
+				"/Users/bill/projects/ncats-translator/relations/craft-relations/CRAFT_aggregate_OWL_ontology.owl");
+		File positivesOutputFile = new File(
+				"/Users/bill/projects/ncats-translator/relations/craft-relations/bert-training-files/craft-assertions.pos.bert");
+		File negativesOutputFile = new File(
+				"/Users/bill/projects/ncats-translator/relations/craft-relations/bert-training-files/craft-assertions.neg.bert");
+
+		try {
+			Set<String> relationIrisOfInterest = new HashSet<String>(Arrays.asList(
+					"http://ccp.cuanschutz.edu/obo/ext/bears_constitution_of_or_situatedness_at_or_possession_by_or_derivation_from"));
+
+			// in order to reduce the number of labels, we will label things at the top-most
+			// level specified by the input relations of interest. This map will be used
+			// when writing the BERT training files to assign an appropriate label, e.g. the
+			// super-property for any sub-property
+			Map<String, String> relationToLabelMap = new HashMap<String, String>();
+			relationIrisOfInterest = addSubProperties(relationIrisOfInterest, craftOntologyFile, relationToLabelMap);
+
+			Set<String> relationsOfInterest = removeNamespaces(relationIrisOfInterest);
+
+			CraftToBertRelationTrainingFile.createBertTrainingFile(craftBaseDirectory, craftAnnotationDirectory,
+					relationsOfInterest, positivesOutputFile, negativesOutputFile, relationToLabelMap);
+		} catch (IOException | OWLOntologyCreationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * The relations are used in the CRAFT annotation files without namespaces, so
+	 * this method returns a list of the relations minus the namespaces
+	 * 
+	 * @param relationsOfInterest
+	 * @return
+	 */
+	private static Set<String> removeNamespaces(Set<String> relationIris) {
+		Set<String> relations = new HashSet<String>();
+
+		for (String iri : relationIris) {
+			relations.add(removeNamespace(iri));
+		}
+
+		return relations;
+	}
+
+	private static String removeNamespace(String iri) {
+		if (iri.startsWith(CCP_EXT_NS)) {
+			return StringUtils.removePrefix(iri, CCP_EXT_NS);
+		} else if (iri.startsWith(OBO_NS)) {
+			return StringUtils.removePrefix(iri, OBO_NS);
+		} else if (iri.startsWith(OWL_UNNAMED_NS)) {
+			return StringUtils.removePrefix(iri, OWL_UNNAMED_NS);
+		} else {
+			throw new IllegalArgumentException("Unexpected namespace on object property: " + iri);
+		}
+	}
+
+	/**
+	 * TODO - this needs to be recursive
+	 * 
+	 * @param relationsOfInterest
+	 * @param craftOntologyFile
+	 * @return
+	 * @throws OWLOntologyCreationException
+	 * @throws IOException
+	 */
+	private static Set<String> addSubProperties(Set<String> relationsOfInterest, File craftOntologyFile,
+			Map<String, String> propToLabelMap) throws OWLOntologyCreationException, IOException {
+
+		System.out.println("Adding subproperties for relations of interest");
+		OntologyUtil ontUtil = new OntologyUtil(craftOntologyFile);
+		OWLOntology ont = ontUtil.getOnt();
+		Set<OWLObjectProperty> objectProperties = ont.getObjectPropertiesInSignature();
+
+		Set<String> relationsWithSubProperties = new HashSet<String>();
+		for (String relation : relationsOfInterest) {
+			System.out.println("Relation: " + relation);
+			boolean existsInOntology = false;
+			for (OWLObjectProperty prop : objectProperties) {
+				String iri = prop.getIRI().toString();
+				if (relationsOfInterest.contains(iri)) {
+					existsInOntology = true;
+					relationsWithSubProperties.add(relation);
+					Set<String> subProperties = getSubProperties(prop, ont);
+					relationsWithSubProperties.addAll(subProperties);
+
+					propToLabelMap.put(removeNamespace(relation), removeNamespace(relation));
+					for (String subProp : subProperties) {
+						propToLabelMap.put(removeNamespace(subProp), removeNamespace(relation));
+					}
+
+//					Set<OWLObjectPropertyExpression> subProperties = prop.getSubProperties(ont);
+//					for (OWLObjectPropertyExpression subProp : subProperties) {
+//						OWLObjectProperty property = subProp.asOWLObjectProperty();
+//						relationsWithSubProperties.add(property.getIRI().toString());
+//						System.out.println("Adding subproperty: " + property.getIRI().toString());
+//					}
+				}
+			}
+			if (!existsInOntology) {
+				throw new IllegalArgumentException("Relation not found in ontology: " + relation);
+			}
+		}
+
+		ontUtil.close();
+
+		return relationsWithSubProperties;
+	}
+
+	private static Set<String> getSubProperties(OWLObjectProperty prop, OWLOntology ont) {
+		Set<String> props = new HashSet<String>();
+		Set<OWLObjectPropertyExpression> subProperties = prop.getSubProperties(ont);
+		for (OWLObjectPropertyExpression subProp : subProperties) {
+			OWLObjectProperty property = subProp.asOWLObjectProperty();
+			props.add(property.getIRI().toString());
+			System.out.println("Adding subproperty: " + property.getIRI().toString());
+			props.addAll(getSubProperties(property, ont));
+		}
+
+		return props;
+	}
+
+}

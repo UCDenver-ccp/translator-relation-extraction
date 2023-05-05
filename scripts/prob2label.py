e3be5975cbddd50fd28dc95f6c3051d596499e8e
Bill Baumgartner <bill.baumgartner@gmail.com>
2023-05-05 11:50:25 -0600

    Update prob2label.py

--- scripts/prob2label.py
+++ scripts/prob2label.py
@@ -35,6 +35,15 @@ elif model_key == 'craft_relations':
     labels[2]="realizes_or_attribute_or_bearer_of_occurrence_in_or_possession_by"
     labels[3]="realizes_or_bears_or_is_attribute_of_derivation_or_situatedness_or_possession_from"
     labels[4]="false"
+elif model_key == 'craft_influence':
+    labels[0]="coexists_as"
+    labels[1]="occurrence_or_attribute_or_effect_brought_about_or_carried_out_by"
+    labels[2]="occurrence_or_attribute_or_bearer_of_influence_on"
+    labels[3]="has_effect_of_causal_activity"
+    labels[4]="has_attribute_of_being_agentive_or_causal_or_its_realization_with"
+    labels[5]="has_attribute_of_being_influenced_or_its_realization_with"
+    labels[6]="false"
+
 # elif model_key == 'bl_gene_to_go_term':
 #   labels[0]="????"
 #   labels[1]="????"

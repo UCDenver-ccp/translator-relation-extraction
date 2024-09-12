4d8675ed85ca0dcfcaa2e09666c7d8cabac2f897
Bill Baumgartner <bill.baumgartner@gmail.com>
2024-09-12 10:53:58 -0400

    Update cat_bert_output.entrypoint.sh

--- scripts/cat_bert_output.entrypoint.sh
+++ scripts/cat_bert_output.entrypoint.sh
@@ -7,7 +7,7 @@ sentence_version=$4
 model_version=$5
 
 # gs://translator-text-workflow-dev_work/output/classified_sentences/sent_0.6.0/bl_chemical_to_cell/model_0.4/filtered/bl_chemical_to_cell.0.6.0_0.4.PMC_SUBSET_31.classified_sentences.filtered-00000-of-00383.tsv.gz
-input_bucket="${resources_bucket}/output/classified_sentences/sent_${sentence_version}/${association_key_lc}/model_${model_version}/${collection}/filtered"
+input_bucket="${resources_bucket}/output/classified_sentences/sent_${sentence_version}/${association_key_lc}/model_${model_version}/filtered/${collection}"
 sentence_file_prefix="${input_bucket}/${association_key_lc}.${sentence_version}_${model_version}.${collection}.classified_sentences.*.gz"
 
 aggregated_bert_output_file_name="bert-output-${association_key_lc}.sent_${sentence_version}.model_${model_version}.${collection}.tsv.gz"
